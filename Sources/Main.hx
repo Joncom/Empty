c8d0bb9f4a92f51175a041a86b37e40230c44093
Jonathan Commins <joncom@gmail.com>
2019-01-10 19:01:12 +0700

    Refine demo

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -2,16 +2,24 @@ package;
 
 class Main {
 	public static function main() {
-		kha.System.start({}, init);
-	}
+		kha.System.start({}, function init(window) {
+			kha.Assets.loadEverything(function() {
+				var image:kha.Image = kha.Assets.images.pixelmix_hd_4x;
 
-	private static function init(window) {
-		kha.Assets.loadEverything(function() {
-			var image:kha.Image = kha.Assets.images.custom_04b03;
-			var copy:kha.Image = kha.Image.createRenderTarget(image.width, image.height);
-			copy.g2.begin(true, kha.Color.Transparent);
-			copy.g2.drawImage(image, 0, 0);
-			copy.g2.end();
+				var copy:kha.Image = kha.Image.createRenderTarget(image.width, image.height);
+				copy.g2.begin(true, kha.Color.Transparent);
+				copy.g2.drawImage(image, 0, 0);
+				copy.g2.end();
+
+				copy.g2.begin(false);
+				copy.g2.end();
+
+				kha.System.notifyOnFrames(function render(framebuffers:Array<kha.Framebuffer>):Void {
+					var framebuffer = framebuffers[0];
+					framebuffer.g2.begin();
+					framebuffer.g2.end();
+				});
+			});
 		});
 	}
 }
