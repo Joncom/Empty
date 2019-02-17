d006173e0403eb3353c2ec3cd5dfae51c77c71ed
Jonathan Commins <joncom@gmail.com>
2019-02-17 16:19:24 +0700

    Print color of white pixel to console (unexepected value)

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -18,6 +18,7 @@ class Main {
 		System.start({title: "Kha", width: 800, height: 600}, function (_) {
 			// Just loading everything is ok for small projects
 			Assets.loadEverything(function () {
+				trace("Color: " + kha.Assets.images.white.at(0, 0)); // Prints -1 (correct) w/ WebGL, and 255 (incorrect) with Canvas
 				// Avoid passing update/render directly,
 				// so replacing them via code injection works
 				Scheduler.addTimeTask(function () { update(); }, 0, 1 / 60);
--- khafile.js
+++ khafile.js
@@ -1,5 +1,7 @@
 let project = new Project('Empty');
 
 project.addSources('Sources');
+project.addAssets('Assets/**', { readable: true });
+project.targetOptions.html5.webgl = false;
 
 resolve(project);
